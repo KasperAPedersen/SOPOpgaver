2c4b2b3f1fec6c8779bc762e1808f1ffd944505d
Kasper Doe <kasper@doe.dk>
2024-11-11 14:19:41 +0100

    Added basic elements

---
+++ ProduktDatabase/ProduktDatabase/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProduktDatabase;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        // ny produkt
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        // rediger produkt
+    }
+
+    private void Button_Click_2(object sender, RoutedEventArgs e)
+    {
+        // slet produkt
+    }
+
+    private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+    {
+        // datagrid
+    }
+}
