bdb05f3ac32a5d7183b777f6f2662743ae2b6382
Kasper Doe <kasper@doe.dk>
2025-02-28 12:32:08 +0100

    :)

---
+++ Menu/Menu/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Menu;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CMenu menu = new CMenuBuilder()
+            .AddItem(new CMenu.MenuOption("Option 1", () =>
+            {
+                CMenu menu = new CMenuBuilder()
+                    .AddItem(new CMenu.MenuOption("Option 1.1", () =>
+                    {
+                        Console.WriteLine("Option 1.1");
+                        Console.ReadKey();
+                    }))
+                    .AddItem(new CMenu.MenuOption("Option 1.2", () =>
+                    {
+                        Console.WriteLine("Option 1.2");
+                        Console.ReadKey();
+                    }))
+                    .Build();
+                
+                menu.Run();
+            }))
+            .AddItem(new CMenu.MenuOption("Option 2", () =>
+            {
+                CMenu menu = new CMenuBuilder()
+                    .AddItem(new CMenu.MenuOption("Option 2.1", () =>
+                    {
+                        Console.WriteLine("Option 2.1");
+                        Console.ReadKey();
+                    }))
+                    .AddItem(new CMenu.MenuOption("Option 2.2", () =>
+                    {
+                        Console.WriteLine("Option 2.2");
+                        Console.ReadKey();
+                    }))
+                    .Build();
+                
+                menu.Run();
+            }))
+            .AddItem(new CMenu.MenuOption("Option 3", () =>
+            {
+                CMenu menu = new CMenuBuilder()
+                    .AddItem(new CMenu.MenuOption("Option 3.1", () =>
+                    {
+                        Console.WriteLine("Option 3.1");
+                        Console.ReadKey();
+                    }))
+                    .AddItem(new CMenu.MenuOption("Option 3.2", () =>
+                    {
+                        Console.WriteLine("Option 3.2");
+                        Console.ReadKey();
+                    }))
+                    .Build();
+                
+                menu.Run();
+            }))
+            .Build();
+        
+        menu.Run();
+
+        Console.ReadKey();
+    }
+}
